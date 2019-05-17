b11c6b27f41f0e8113724f72aeb43a63b4d2b870
imagineSolo <48104451+imagineSolo@users.noreply.github.com>
2019-05-18 00:46:51 +0200

    remove method/info-box method

--- Army Builder/app.js
+++ Army Builder/app.js
@@ -5,6 +5,14 @@ class Unit {
     this.value = value;
     this.img = img;
   }
+  addGeneral() {
+    const army = document.querySelector('.new-army');
+    const reinforcement = document.createElement('div');
+    reinforcement.classList.add('general');
+    reinforcement.innerHTML = `<img src=${this.img}>`;
+    reinforcement.id = `${this.type}`;
+    army.appendChild(reinforcement);
+  }
   addUnit() {
     const army = document.querySelector('.new-army');
     const reinforcement = document.createElement('div');
@@ -12,53 +20,120 @@ class Unit {
     reinforcement.innerHTML = `<img src=${this.img}>`;
     reinforcement.id = `${this.type}`;
     army.appendChild(reinforcement);
-
+  }
+  removeUnit(target) {
+    if (target.parentElement.className === 'unit') {
+      target.parentElement.remove();
+    }
+  }
+  showInfo() {
+    const info = document.querySelector('.info-box span');
+    info.innerHTML = `Unit: ${this.name} 
+                      Type: ${this.type}
+                      Value: ${this.value}`;
+  }
+  clearInfo() {
+    const info = document.querySelector('.info-box span');
+    info.innerHTML = '';
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   const general = new Unit('General', 'general', 10, 'img/general.jpg');
-  general.addUnit();
+  general.addGeneral();
 })
 
-document.getElementById('knights').addEventListener('click', (e) => {
+document.getElementById('knights').addEventListener('click', () => {
   const knights = new Unit('Knights', 'knights', 5, 'img/knights.jpg');
   knights.addUnit();
-  e.preventDefault();
-  console.log(knights);
 })
-document.getElementById('light-cavalry').addEventListener('click', (e) => {
+document.getElementById('light-cavalry').addEventListener('click', () => {
   const lightCavalry = new Unit('Light Cavalry', 'light-cavalry', 3, 'img/light-cavalry.jpg');
   lightCavalry.addUnit();
-  e.preventDefault();
 })
 document.getElementById('heavy-infantry').addEventListener('click', (e) => {
   const heavyInfantry = new Unit('Heavy Infantry', 'heavy-infantry', 4, 'img/heavy-infantry.jpg');
   heavyInfantry.addUnit();
-  e.preventDefault();
 })
-document.getElementById('light-infantry').addEventListener('click', (e) => {
+document.getElementById('light-infantry').addEventListener('click', () => {
   const lightInfantry = new Unit('Light Infantry', 'light-infantry', 1, 'img/light-infantry.jpg');
   lightInfantry.addUnit();
-  e.preventDefault();
 })
-document.getElementById('bowmen').addEventListener('click', (e) => {
+document.getElementById('bowmen').addEventListener('click', () => {
   const bowmen = new Unit('Bowmen', 'bowmen', 1, 'img/bowmen.jpg');
   bowmen.addUnit();
-  e.preventDefault();
 })
-document.getElementById('peasant-bowmen').addEventListener('click', (e) => {
+document.getElementById('peasant-bowmen').addEventListener('click', () => {
   const peasantBowmen = new Unit('Peasant Bowmen', 'peasant-bowmen', 1, 'img/peasant-bowmen.jpg');
   peasantBowmen.addUnit();
-  e.preventDefault();
 })
-document.getElementById('catapult').addEventListener('click', (e) => {
+document.getElementById('catapult').addEventListener('click', () => {
   const catapult = new Unit('Catapult', 'catapult', 4, 'img/catapult.jpg');
   catapult.addUnit();
-  e.preventDefault();
 })
-document.getElementById('trebuchet').addEventListener('click', (e) => {
+document.getElementById('trebuchet').addEventListener('click', () => {
   const trebuchet = new Unit('Trebuchet', 'trebuchet', 5, 'img/trebuchet.jpg');
   trebuchet.addUnit();
-  e.preventDefault();
+})
+
+document.querySelector('.new-army').addEventListener('click', (e) => {
+  const unit = new Unit;
+  unit.removeUnit(e.target);
+})
+
+document.querySelector('#knights').addEventListener('mouseover', (e) => {
+  const knights = new Unit('Knights', 'knights', 5, 'img/knights.jpg');
+  knights.showInfo(e.target);
+  document.querySelector('#knights').addEventListener('mouseleave', () => {
+    knights.clearInfo();
+  })
+})
+document.querySelector('#light-cavalry').addEventListener('mouseover', (e) => {
+  const lightCavalry = new Unit('Light Cavalry', 'light-cavalry', 3, 'img/light-cavalry.jpg');
+  lightCavalry.showInfo(e.target);
+  document.querySelector('#light-cavalry').addEventListener('mouseleave', () => {
+    lightCavalry.clearInfo();
+  })
+})
+document.querySelector('#heavy-infantry').addEventListener('mouseover', (e) => {
+  const heavyInfantry = new Unit('Heavy Infantry', 'heavy-infantry', 4, 'img/heavy-infantry.jpg');
+  heavyInfantry.showInfo(e.target);
+  document.querySelector('#heavy-infantry').addEventListener('mouseleave', () => {
+    heavyInfantry.clearInfo();
+  })
+})
+document.querySelector('#light-infantry').addEventListener('mouseover', (e) => {
+  const lightInfantry = new Unit('Light Infantry', 'light-infantry', 1, 'img/light-infantry.jpg');
+  lightInfantry.showInfo(e.target);
+  document.querySelector('#light-infantry').addEventListener('mouseleave', () => {
+    lightInfantry.clearInfo();
+  })
+})
+document.querySelector('#bowmen').addEventListener('mouseover', (e) => {
+  const bowmen = new Unit('Bowmen', 'bowmen', 1, 'img/bowmen.jpg');
+  bowmen.showInfo(e.target);
+  document.querySelector('#bowmen').addEventListener('mouseleave', () => {
+    bowmen.clearInfo();
+  })
+})
+document.querySelector('#peasant-bowmen').addEventListener('mouseover', (e) => {
+  const peasantBowmen = new Unit('Peasant Bowmen', 'peasant-bowmen', 1, 'img/peasant-bowmen.jpg');
+  peasantBowmen.showInfo(e.target);
+  document.querySelector('#peasant-bowmen').addEventListener('mouseleave', () => {
+    peasantBowmen.clearInfo();
+  })
+})
+document.querySelector('#catapult').addEventListener('mouseover', (e) => {
+  const catapult = new Unit('Catapult', 'catapult', 4, 'img/catapult.jpg');
+  catapult.showInfo(e.target);
+  document.querySelector('#catapult').addEventListener('mouseleave', () => {
+    catapult.clearInfo();
+  })
+})
+document.querySelector('#trebuchet').addEventListener('mouseover', (e) => {
+  const trebuchet = new Unit('Trebuchet', 'trebuchet', 5, 'img/trebuchet.jpg');
+  trebuchet.showInfo(e.target);
+  document.querySelector('#trebuchet').addEventListener('mouseleave', () => {
+    trebuchet.clearInfo();
+  })
 })
